a7c1a6db801d73d1b82fb31cc5b4474d64ca1fcd
gvssmark <83894487+gvssmark@users.noreply.github.com>
2026-08-09 04:30:18 +0530

    update all

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // Bump CACHE_VERSION whenever app.js/index.html/CSS changes, so old
 // clients pick up the new files promptly instead of waiting on the
 // background revalidation alone.
-var CACHE_VERSION = 'raghu-v4';
+var CACHE_VERSION = 'raghu-v5';
 
 var APP_SHELL = [
   './index.html',
